5a859d8120442f794a70b6da1814103885f316cd
Moe <moeali00000@gmail.com>
2023-08-10 12:38:38 -0700

    mine

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Start
+ *
+ * Description: useless shit
+ *
+ * Return: always 1
+*/
+
+int main(void)
+{
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, c, 59);
+	return (1);
+}
