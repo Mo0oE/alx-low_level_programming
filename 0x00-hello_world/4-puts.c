186298147211f580cb035fd2293af42b2fb46f67
Moe <moeali00000@gmail.com>
2023-08-10 11:35:49 -0700

    mine

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Printing a string'
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
