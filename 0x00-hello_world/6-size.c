daaa6f2f0e90c2d7155ba69d70c84107f7c6ac5a
Moe <moeali00000@gmail.com>
2023-08-10 12:20:49 -0700

    mine

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Start
+ *
+ * Desciption: a useless program
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	printf("Size of a char: %i byte(s)", sizeof(char));
+	printf("Size of an int: %i byte(s)", sizeof(int));
+	printf("Size of a long int: %i byte(s)", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)", sizeof(long long int));
+	printf("Size of a float: %i byte(s)", sizeof(float));
+}
