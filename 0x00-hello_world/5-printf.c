f6d9f6287cda9d5dcb5e61dc59f7d0ed23df4a36
Moe <moeali00000@gmail.com>
2023-08-10 12:10:17 -0700

    mine

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
+
 /**
- * Main: the start
- * Descreption: "prints"
- * return = 0 always
+ * main -  the start
+ *
+ * Description: "prints"
+ *
+ * Return: 0 always
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
