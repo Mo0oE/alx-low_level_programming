944fadc99d8b4e6f4c0e5b5174541b985ea68dfb
Moe <moeali00000@gmail.com>
2023-08-11 05:09:48 -0700

    mine

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - start
+ *
+ * Description: None
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char c = 'z';
+
+	for (; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+
+	return (0);
+}
