656f692e08125cc55c5eb4d68c80265728a6ffae
Moe <moeali00000@gmail.com>
2023-08-11 06:38:41 -0700

    mine

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/**
+ * main - start
+ *
+ * Description: None
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int c = 0, c2;
+
+	for (; c <= 9; c++)
+	{
+		for (c2 = c; c2 <= 9; c2++)
+		{
+			putchar(c + '0');
+			putchar(c2 + '0');
+			if (c != 8 && c2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+
+	return (0);
+}
