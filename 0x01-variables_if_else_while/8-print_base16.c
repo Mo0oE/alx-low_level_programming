d673eef5c748f75adeeaa682deeceec799d7a190
Moe <moeali00000@gmail.com>
2023-08-11 05:13:05 -0700

    mine

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - start
+ *
+ * Description: None
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char c = '0', c6 = 'a';
+
+	for (; c <= '9'; c++)
+	{
+		printf(c);
+	}
+	for (; c6 <= 'f'; c6++)
+	{
+		putchar(c6;
+	}
+	putchar('\n');
+
+
+	return (0);
+}
