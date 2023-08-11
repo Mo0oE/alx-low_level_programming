33b2bebe562f55ed2b2227cdeb80aec20aa73e1d
Moe <moeali00000@gmail.com>
2023-08-11 06:51:27 -0700

    mine

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,11 +12,11 @@ int main(void)
 {
 	int c = 0, c2, c3;
 
-	for (; c <= 9; c++)
+	for (; c <= 7; c++)
 	{
-		for (c2 = c + 1; c2 <= 7; c2++)
+		for (c2 = c + 1; c2 <= 8; c2++)
 		{
-			for (c3 = c2 + 1; c3 <= 8; c3++)
+			for (c3 = c2 + 1; c3 <= 9; c3++)
 			{
 				putchar(c + '0');
 				putchar(c2 + '0');
