5d4ef7b690a0e0635d37edf669495a22857d4772
Moe <moeali00000@gmail.com>
2023-08-11 05:04:17 -0700

    mine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - start
+ *
+ * Description: None
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int c = 0;
+
+	for (; c <= 9; c++)
+	{
+		printf("%i", c);
+	}
+	putchar('\n');
+
+
+	return (0);
+}
