312280961ff7ad48c1753d8e7b8ae6a4f0b756e6
Moe <moeali00000@gmail.com>
2023-08-15 06:01:41 -0700

    mine

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - the function
+ * @c: the input
+ * Return: 0 or 1
+*/
+
+int _islower(int c)
+{
+	if (c <= 122 && c >= 97)
+		return (1);
+	else
+		return (0);
+
+}
