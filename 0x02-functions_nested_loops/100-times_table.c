d642de0ef9b8d53907b14dcdbafd4ed983bf5ef2
Moe <moeali00000@gmail.com>
2023-08-15 09:23:37 -0700

    mine

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_times_table - function
+ * @n: input
+ * Return: None
+*/
+
+void print_times_table(int n)
+{
+	int c, r = 0, res = 0;
+
+	for (; r <= n; r++)
+	{
+		_putchar ('0');
+		for (c = 1; c <= n; c++)
+		{
+			res = c  * r;
+
+			if (res < 10)
+			{
+				_putchar (',');
+				_putchar (' ');
+				_putchar (' ');
+				_putchar (' ');
+				_putchar ('0' + res);
+			}
+			else if (res < 100)
+			{
+				_putchar (',');
+				_putchar (' ');
+				_putchar ('0' + (res / 10));
+				_putchar ('0' + (res % 10));
+			}
+			else
+			{
+				_putchar (',');
+				_putchar (' ');
+				_putchar ('0' + (res / 100));
+				_putchar ('0' + ((res % 100) / 10));
+				_putchar ('0' + (res % 10));
+			}
+		}
+		_putchar ('\n');
+	}
+
+
+
+}
