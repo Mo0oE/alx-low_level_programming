afed0626a8f4ea0a5459590603464d19d3c1f8ee
Moe <moeali00000@gmail.com>
2023-08-15 11:48:20 -0700

    mine

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,17 +10,17 @@
 int main(void)
 {
 	int i = 2;
-	unsigned long long int nums[50];
+	unsigned long int nums[50];
 
 	nums[0] = 1;
 	nums[1] = 2;
 
-	printf("%llu, %llu", nums[0], nums[1]);
+	printf("%lu, %lu", nums[0], nums[1]);
 
 	for (; i < 50; i++)
 	{
 		nums[i] = nums[i - 2] + nums[i - 1];
-		printf(", %llu", nums[i]);
+		printf(", %lu", nums[i]);
 	}
 	printf("\n");
 	return (0);
