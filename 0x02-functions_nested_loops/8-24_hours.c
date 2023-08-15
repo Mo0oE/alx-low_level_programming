da8f32be32cf2d8c39de5a5b47056ade86aaf1ae
Moe <moeali00000@gmail.com>
2023-08-15 07:51:46 -0700

    mine

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,28 @@
 #include "main.h"
 
-/***/
+/**
+ * jack_bauer - function
+ *
+ * Return: None
+*/
 
 void jack_bauer(void)
 {
+	int min, hr = 0;
+
+	for (; hr < 24; hr++)
+	{
+
+		for (min = 0; min < 60; min++)
+		{
+			_putchar ('0' + (hr / 10);
+			_putchar ('0' + (hr % 10);
+			_putchar (':');
+			_putchar ('0' + (min / 10);
+			_putchar ('0' + (min % 10);
+			_putchar ('\n');
+		}
+
+	}
 
 }
