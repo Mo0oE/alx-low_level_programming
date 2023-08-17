6431606b0c33759c144d33a36b124208a0eb1db2
Moe <moeali00000@gmail.com>
2023-08-17 09:34:59 -0700

    mine

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,15 +5,17 @@
  * Description: None
 */
 
-void print_numbers(void)
+void more_numbers(void)
 {
-	int i;
+	int i, y;
 
 	for (y = 0; y < 10; y++)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			_putchar ('0' + i);
+			if (i >= 10)
+				_putchar ('1');
+			_putchar ('0' + i % 10);
 		}
 		_putchar ('\n');
 	}
