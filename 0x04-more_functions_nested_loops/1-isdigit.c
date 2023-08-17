a514515839c21594fdc9467f58e4affd26372162
Moe <moeali00000@gmail.com>
2023-08-16 20:40:18 -0700

    mine

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - function
+ * _isdigit - function
  * @c: input
  * Description: None
  * Return: 0 or 1
 */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function
+ * @a: input
+ * @b: input
+ * Return: the value
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
