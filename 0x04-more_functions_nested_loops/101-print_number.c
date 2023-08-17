b2e8b51e320ec82571e1ceacc58e3674ea4e0876
Moe <moeali00000@gmail.com>
2023-08-17 14:38:33 -0700

    mine

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,25 +8,13 @@
 
 void print_number(int n)
 {
-	int n1, c, x;
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
-	n1 = n;
-	c = 0;
+	if ((num / 10) > 0)
+		print_number(n / 10);
 
-	while (n1 > 0)
-	{
-		n1 /= 10;
-		c++;
-	}
-
-	for (; c >= 1; c--)
-	{
-		x = n / (10 ^ c);
-		_putchar('0' + x);
-		n = n % (10 ^ c);
-	}
+	_putchar('0' + (n % 10));
 }
