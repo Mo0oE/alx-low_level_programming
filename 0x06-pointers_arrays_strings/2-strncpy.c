139ce1dc1fa9345e755f427b317d11e5023e52fd
Moe <moeali00000@gmail.com>
2023-08-23 20:35:41 -0700

    mine

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - function
+ * @dest: input
+ * @src:input
+ * @n: input
+ * Description: none
+ * Return: smtg
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src [i];
+		i++;
+	}
+	while (n > i)
+		dest[i++]='\0';
+
+	return (dest);
+}
