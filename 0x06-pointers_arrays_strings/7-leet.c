d3cf42508d0cb7a880bb69dbc4d8d525357fe95d
Moe <moeali00000@gmail.com>
2023-08-24 04:23:47 -0700

    mine

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,17 +9,18 @@
 
 char *leet(char *str)
 {
-	char *c = {'A', 'E', 'O', 'T', 'L'};
-	char *n = {'4', '3', '0', '7', '1'};
-	int i = 0, y = 0;
+	char c[] = {'A', 'E', 'O', 'T', 'L'};
+	char n[] = {'4', '3', '0', '7', '1'};
+	int i = 0, y;
 
 	while (str[i])
 	{
-		for (; y < 5; y++)
+		for (y = 0; y < 5; y++)
 		{
 			if (str[i] == c[y] || str[i] == c[y] + 32)
 				str[i] = n[y];
 		}
+		i++;
 	}
 	return (str);
 }
