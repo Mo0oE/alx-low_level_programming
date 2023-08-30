25f354d48454b570f8a4b98c05d20a1a55351188
Moe <moeali00000@gmail.com>
2023-08-30 07:10:01 -0700

    mine

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - fu
+ * @x: hl
+ * @y: jof
+ * Return: n;olihj
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (x);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
