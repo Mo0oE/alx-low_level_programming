c56c0bb2e0f4505f8a32e0b5bb0d5390b2d3ab82
Moe <moeali00000@gmail.com>
2023-08-30 06:06:59 -0700

    mine

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <strings.h>
+
+/**
+ * _puts_recursion - fu
+ * @s: in
+ * Description: none
+*/
+
+void _puts_recursion(char *s)
+{
+	if (&s != '\0')
+	{
+		_putchar (&s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar ('\0');
+		_putchar ('\n');
+	}
+}
