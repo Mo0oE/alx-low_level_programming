dd0d2f3f66636ac2007ca77782642a3c8248566e
Moe <moeali00000@gmail.com>
2023-09-01 19:32:35 -0700

    mine

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - fu
+ * @argc: in
+ * @argv: arr
+ * Return: 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
