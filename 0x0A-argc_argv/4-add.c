b60fe4f1e5798a137b076de7ac430c9fb22d11b2
Moe <moeali00000@gmail.com>
2023-09-01 20:00:06 -0700

    mine

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - fu
+ * @argc: in
+ * @argv: arr
+ * Return: 0
+*/
+
+int main(int argc, char *argv)
+{
+	int sum = 0, i = 1;
+	char *check;
+
+	for (; i < argc; i++)
+	{
+		for (check = argv[i]; *check; check++)
+			if (*check > '9' || *check < '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
