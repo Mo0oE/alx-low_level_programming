2c54bcbe0e794d0752dd6a4238d8023360546ca5
Moe <moeali00000@gmail.com>
2023-09-01 19:45:16 -0700

    mine

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - fu
+ * @argc: in
+ * @argv: arr
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int a = atoi(argv[1]), b = atoi(argv[2]);
+
+		printf("%d\n", a + b);
+		return (0);
+	}
+}
