f424a65f114250d7afd4724a2016ced623918518
Moe <moeali00000@gmail.com>
2023-09-04 05:15:06 -0700

    mine

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * alloc_grid - fu
+ * @width: in
+ * @height: in
+ * Return: ointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, y;
+	int **allgrid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	allgrid = calloc(height, sizeof(int));
+
+	if (allgrid == NULL)
+		return (0);
+
+	for (i = 0; i < height; i++)
+	{
+		allgrid[i] = calloc(width, sizeof(int));
+		if(allgrid[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(allgrid[i]);
+			free(allgrid);
+			return (NULL);
+		}
+	}
+	return (allgrid);
+}
