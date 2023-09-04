4bcc24303a0d762e5fa3a23931228ad31ec58065
Moe <moeali00000@gmail.com>
2023-09-04 04:28:24 -0700

    mine

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,16 +9,24 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int si1, si2,size, i = 0, y = 0;
+	int si1, si2, size, i = 0, y = 0;
 	char *str;
 
 	if (!s1 && !s2)
 		return (NULL);
-	si1 = strlen(s1);
-	si2 = strlen(s2);
+	if (!s1)
+		si1 = 0;
+	else
+		si1 = strlen(s1);
+	if (!s2)
+		si2 = 0;
+	else
+		si2 = strlen(s2);
 	size = si1 + si2;
 	str = (char *) malloc(size + 1);
 
+	if (str == NULL)
+		return (NULL);
 	for (; i < size; i++)
 	{
 		if (i < si1)
@@ -29,6 +37,6 @@ char *str_concat(char *s1, char *s2)
 			y++;
 		}
 	}
-	str[size] ='\0';
+	str[size] = '\0';
 	return (str);
 }
