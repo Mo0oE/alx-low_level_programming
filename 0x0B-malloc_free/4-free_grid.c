7802a044ee9ff9890f1aee405947e36d7b931cc3
Moe <moeali00000@gmail.com>
2023-09-04 06:20:31 -0700

    mine

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * free_grid _ fu
+ * @grid: in
+ * @height: in
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
