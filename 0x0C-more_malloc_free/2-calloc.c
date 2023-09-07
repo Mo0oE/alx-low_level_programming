801668f4fc0f0d050383c3a444aea2fc3732a51e
Moe <moeali00000@gmail.com>
2023-09-07 03:48:05 -0700

    mine

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(nmemb * size);
+	arr = malloc((nmemb + 1) * size);
 
 	if (arr == NULL)
 		return (NULL);
