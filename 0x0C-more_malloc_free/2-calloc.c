95e9cc69695c46b66e7ebb473a3bd2974f824474
Moe <moeali00000@gmail.com>
2023-09-07 03:42:38 -0700

    mine

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _calloc - fu
+ * @nmemb: in
+ * @size: in
+ * Return: none
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *arr;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(nmemb * size);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		arr[i] = 0;
+	}
+	return (arr);
+}
