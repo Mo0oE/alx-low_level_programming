933f5388f8eec204f94c643b634c25b3ba95f910
Moe <moeali00000@gmail.com>
2023-09-07 03:20:52 -0700

    mine

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * string_nconcat - fu
+ * @s1: in
+ * @s2: in
+ * @n: in
+ * Return: Nnon
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int size1 = strlen(s1), size = size1 + strlen(s2), i, y = 0;
+	char *str;
+
+	if (size > (size1 + n))
+		size = size1 + n;
+
+	str = malloc(size + 1);
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < size1 ; i++)
+	{
+		str[y] = s1[i];
+		y++;
+	}
+	for(i = 0; i < (size - size1); i++)
+	{
+		str[y] = s2[i];
+		y++;
+	}
+	str[y] = '\0';
+	return (str);
+}
+
