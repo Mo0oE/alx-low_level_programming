29eb04b909ff375fb6e1d764535561304efdfcb8
Moe <moeali00000@gmail.com>
2023-09-07 02:56:09 -0700

    mine

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
-/***/
+/**
+ * malloc_checked - fu
+ * @b: in
+ * Return: Nnon;
+*/
 
 void *malloc_checked(unsigned int b)
 {
-	malloc(b);
-	if (b == NULL)
+	int *s = malloc(b);
+
+	if (s == NULL)
 		exit(98);
+	return (s);
 }
