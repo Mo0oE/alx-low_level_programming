bbef55083b30339a0c3359484d4253c5738c2785
Moe <moeali00000@gmail.com>
2023-09-11 11:46:06 -0700

    mine

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - fu
+ * @name: in
+ * @age: in
+ * @owner: in
+*/
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newdog;
+
+	if (!name || age < 0 || !owner)
+		return (NULL);
+
+	newdog = (dog_t *) malloc (sizeof(dog_t));
+	if (!newdog)
+		return (NULL);
+
+	newdog->name = malloc(strlen(name) + 1);
+	if (!newdog->name)
+	{
+		free(newdog);
+		return (NULL);
+	}
+
+	newdog->owner = malloc(strlen(owner) + 1);
+	if (!newdog->owner)
+	{
+		free(newdog->name);
+		free(newdog);
+		return (NULL);
+	}
+
+	newdog->age = age;
+	newdog->name = strcpy(newdog->name, name);
+	newdog->owner = strcpy(newdog->owner, owner);
+
+	return (newdog);
+}
