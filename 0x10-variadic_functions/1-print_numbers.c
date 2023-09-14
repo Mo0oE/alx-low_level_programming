6171d2baae7c872426c8baba7517d76651a09935
Moe <moeali00000@gmail.com>
2023-09-14 15:43:43 -0700

    mine

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,6 +13,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	va_list args;
 
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
 	va_start(args, n);
 	for (; i < n - 1; i++)
 		if (separator)
