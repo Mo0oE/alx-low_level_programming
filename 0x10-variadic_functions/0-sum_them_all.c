629e4a841b77af245dc0cd22be552ec865309cc8
Moe <moeali00000@gmail.com>
2023-09-14 15:19:52 -0700

    mine

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - fu
+ * @n: in
+ * @...: ins
+ * Return: sum
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i = 0, sum = 0;
+	va_list args;
+
+	if (!n)
+		return (0);
+	va_start(args, n);
+	for (; i < n; i++)
+		sum += va_arg(args, int);
+	va_end(args);
+	return (sum);
+}
