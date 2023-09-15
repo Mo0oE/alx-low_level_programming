c26184473142af9017cea43acc8718cb42e03ff5
Moe <moeali00000@gmail.com>
2023-09-14 20:23:32 -0700

    mine

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,18 +1,89 @@
 #include "variadic_functions.h"
 
-/***/
+/**
+ * printint - fu
+ * @s: in
+ * @args: in
+ * Description: none
+*/
+
+void printint (char *s, va_list args)
+{
+	printf("%s%d", s, va_arg(args, int));
+}
+
+/**
+ * printfloat - fu
+ * @s: in
+ * @args: in
+ * Description: none
+*/
+
+void printfloat (char *s, va_list args)
+{
+	printf("%s%f", s, va_arg(args, double));
+}
+
+/**
+ * printchar - fu
+ * @s: in
+ * @args: in
+ * Description: none
+*/
+
+void printchar (char *s, va_list args)
+{
+	printf("%s%c", s, va_arg(args, int));
+}
+
+/**
+ * printstring - fu
+ * @s: in
+ * @args: in
+ * Descrtiption: none
+*/
+
+void printstring(char *s, va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (!str)
+		str = "(nil)";
+	printf("%s%s", s, str);
+}
+
+/**
+ * print_all - fu
+ * @format: in
+ * @...: ins
+ * Description: none
+*/
 
 void print_all(const char * const format, ...)
 {
-	int size = strlen(format), i = 0;
+	int size = strlen(format), i = 0, j;
 	va_list args;
-	char c;
 	char *chk = "ifcs";
-	void (*fu[])();
-	
+	char *sep = "";
+
+	void (*fu[])() = {printint, printfloat, printchar, printstring};
+
 	va_start(args, format);
 	while (i < size)
 	{
-		
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == chk[j])
+			{
+				fu[j](sep, args);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
 	}
+	printf("\n");
+	va_end(args);
 }
+
