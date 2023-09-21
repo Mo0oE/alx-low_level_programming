ad5cfdb08b3cb901fae301d605667a78cd912a0e
Moe <moeali00000@gmail.com>
2023-09-21 14:20:10 -0700

    commity

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_list - fu
+ * @head: in
+ * Description: none
+*/
+
+void free_list(list_t *head)
+{
+	list_t *current, *next;
+
+	if(!head)
+		return;
+
+	current = head;
+	while (current)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,18 +22,13 @@ typedef struct list_s
 } list_t;
 
 
-
-
 size_t print_list(const list_t *h);
 int _putchar(char str);
 void print_number(int n);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
-
-
-
-
+void free_list(list_t *head);
 
 
 #endif
