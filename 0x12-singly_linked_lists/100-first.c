3a688ce9aec2c49fa056de31672462e0291067f4
Moe <moeali00000@gmail.com>
2023-09-21 14:30:51 -0700

    commity

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * before-main - fu
+ * Description: none
+ */
+
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
