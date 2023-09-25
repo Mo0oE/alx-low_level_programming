24b1fc6f21768925992c1f49fdd3007d04835d2a
Moe <moeali00000@gmail.com>
2023-09-25 08:29:40 -0700

    commity

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (!head)
+		return;
+
 	while (*head)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
-	free(*head);
-	head = NULL;
+	*head = NULL;
 }
