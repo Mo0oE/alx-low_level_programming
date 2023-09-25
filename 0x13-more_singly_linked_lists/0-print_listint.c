df111ea3c5491a613a10237e1faaa639f97d3144
Moe <moeali00000@gmail.com>
2023-09-25 08:06:29 -0700

    commity

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	 int count = 0;
+	int count = 0;
 
-	 while (h)
-	 {
-		 printf("%i\n", h->n);
-		 count++;
-		 h = h->next;
-	 }
-	 return (count);
+	while (h)
+	{
+		printf("%i\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
