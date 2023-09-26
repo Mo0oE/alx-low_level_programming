f7b7c1a613d3afd62b4d1395b3e32eca1ca65b5b
Moe <moeali00000@gmail.com>
2023-09-26 05:19:45 -0700

    commity

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - fu
+ * @head: in
+ * @index: in
+ * Return: n
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp = head;
+
+	while (index--)
+		tmp = tmp->next;
+	return (tmp->n);
+}
