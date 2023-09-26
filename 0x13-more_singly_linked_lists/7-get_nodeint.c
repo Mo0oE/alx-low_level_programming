40c9fe76344fc06b33f7e41d6ac6c702a92d88e1
Moe <moeali00000@gmail.com>
2023-09-26 05:26:43 -0700

    commity

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,9 +11,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *tmp = head;
 
-
-	if (!head)
-		return (NULL);
 	while (index--)
 		tmp = tmp->next;
 	return (tmp);
