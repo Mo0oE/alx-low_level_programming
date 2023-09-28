b651c73e9a5e7243b22572549e51b926e4552fd7
Moe <moeali00000@gmail.com>
2023-09-28 09:25:39 -0700

    commity

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_bit - fu
+ * @n: in
+ * @index: in
+ * Return: 0 1 -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(*n) * 8)
+		return (-1);
+
+	return ((1 << index) | *n);
+}
