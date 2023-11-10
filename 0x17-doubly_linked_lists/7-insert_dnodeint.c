257deacf8bb8bcb81a30abd1b222cb4f28fc8c8a
Moe <moeali00000@gmail.com>
2023-11-10 02:05:39 +0200

    commity

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,44 +1,39 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - fu
- * @h: list
- * @idx: the index
- * @n: the value
- * Return: pointer
-*/
-
+ * insert_dnodeint_at_index - Insert a node in the postion
+ * @h: Pointer to direction of the head
+ * @n: The data integer
+ * @idx: Position at the insert the new node
+ * Return: The direction of the new node
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = *h, *new;
+	dlistint_t *new_mem, *res_mem = *h;
+	/* unsigned int counter = 0 */
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-
-	while (idx > 1)
+	for (; idx != 1; idx--)
 	{
-		if (!current)
+		if (res_mem == NULL)
 			return (NULL);
-		current = current->next;
-		idx--;
+		res_mem = res_mem->next;
 	}
 
-	if (current->next == NULL)
+	if (res_mem->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	new_mem = malloc(sizeof(dlistint_t));
+
+	if (new_mem == NULL)
 		return (NULL);
 
-	new->n = n;
-	new->prev = current;
-	new->next = current->next;
-	current->next->prev = new;
-	current->next = new;
+	new_mem->n = n;
+	new_mem->next = res_mem->next;
+	new_mem->prev = res_mem;
+	res_mem->next->prev = new_mem;
+	res_mem->next = new_mem;
 
-	return (new);
+	return (new_mem);
 }
